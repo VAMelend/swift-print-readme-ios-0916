41dcd8658e650d304d19ae4e9669be9f77694c56
Victoria Melendez <Victoria@Victorias-MacBook-Pro.local>
2016-08-29 15:34:46 -0400

    thanks

---
+++ VMPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Hello, world"
+
+
+
+
+var favoriteWord = "lumberjack"
+var favoriteColor = "orange"
+var favoriteMusician = "Wilco"
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+
+
